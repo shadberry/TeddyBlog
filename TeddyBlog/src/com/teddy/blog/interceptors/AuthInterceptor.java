93be279aef59375782f2e28933bf48883ff598eb
shadberry <shadberry89@gmail.com>
2013-06-26 11:06:36 +0800

    修改权限验证拦截器
    Signed-off-by: shadberry <shadberry89@gmail.com>

--- TeddyBlog/src/com/teddy/blog/interceptors/AuthInterceptor.java
+++ TeddyBlog/src/com/teddy/blog/interceptors/AuthInterceptor.java
@@ -1,35 +1,39 @@
 package com.teddy.blog.interceptors;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
-import org.apache.struts2.interceptor.SessionAware;
 
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.Interceptor;
 
-public class AuthInterceptor implements Interceptor, SessionAware {
+public class AuthInterceptor implements Interceptor {
 
 	private static final long serialVersionUID = -8966824305616009978L;
 	
-	private Map<String, Object> session;
+	private Map<String, List<String>> forUserMap = new HashMap<String, List<String>>();
 
 	@Override
 	public void destroy() { }
 
 	@Override
-	public void init() { }
+	public void init() { 
+		List<String> methods = new ArrayList<String>();
+		methods.add("write");
+		methods.add("save");
+		methods.add("edit");
+		methods.add("saveEdit");
+		forUserMap.put("article", methods);
+	}
 
 	@Override
 	public String intercept(ActionInvocation invoc) throws Exception {
 //		System.out.println("Action class is "+invoc.getAction().getClass().getName());  
 //		System.out.println("Action name is "+invoc.getProxy().getActionName());  
 //		System.out.println("Method name is "+invoc.getProxy().getMethod()); 
+		
 		return invoc.invoke();
-	}
-
-	@Override
-	public void setSession(Map<String, Object> session) {
-		this.session = session;
 	}
 
 }
